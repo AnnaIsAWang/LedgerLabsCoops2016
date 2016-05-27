ff24f9fbe7494f241c9cd5d3fe14167b87894805
Denton Liu <liu.denton+github@gmail.com>
2016-05-27 15:39:11 -0400

    Changed from tabs to spaces

--- AbstractedStateChannel/Rules.sol
+++ AbstractedStateChannel/Rules.sol
@@ -3,17 +3,17 @@ import "LockedState.sol";
 
 contract Rules {
 
-	event StateChannelCreated(address Adjudicator, address LockedState);
+    event StateChannelCreated(address Adjudicator, address LockedState);
 
-	Adjudicator adjudicator;
+    Adjudicator adjudicator;
 
-	function Rules() {
-		adjudicator = createAdjudicator();
-		StateChannelCreated(adjudicator, adjudicator.getLockedStateAddress());
-	}
+    function Rules() {
+        adjudicator = createAdjudicator();
+        StateChannelCreated(adjudicator, adjudicator.getLockedStateAddress());
+    }
 
-	function createAdjudicator() internal returns (Adjudicator);
+    function createAdjudicator() internal returns (Adjudicator);
 
-	// you may define the rules and exposure conditions of the state channel
-	// in a subcontract that inherits from this contract
+    // you may define the rules and exposure conditions of the state channel
+    // in a subcontract that inherits from this contract
 }
