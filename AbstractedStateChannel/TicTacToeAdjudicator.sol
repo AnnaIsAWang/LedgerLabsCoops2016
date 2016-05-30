e9df501b67633d427f995849bd791fcf9f6b3627
Denton Liu <liu.denton+github@gmail.com>
2016-05-30 11:25:02 -0400

    Added comments for TicTacToeAdjudicator.sol

--- AbstractedStateChannel/TicTacToeAdjudicator.sol
+++ AbstractedStateChannel/TicTacToeAdjudicator.sol
@@ -1,8 +1,20 @@
 import "Adjudicator.sol";
 import "TicTacToeLockedState.sol";
 
+/**
+ * TicTacToeAdjudicator
+ *
+ * Adjudicates a TicTacToe game.
+ */
 contract TicTacToeAdjudicator is Adjudicator {
 
+    /**
+     * Creates a new TicTacToeAdjudicator
+     *
+     * address1: The address of the first player
+     * address2: The address of the second player
+     * _timeout: The amount of time required before the state channel can be closed normally.
+     */
     function TicTacToeAdjudicator(address address1, address address2, uint _timeout)
         Adjudicator(new address[](2), 2, _timeout)
     {
@@ -10,6 +22,7 @@ contract TicTacToeAdjudicator is Adjudicator {
         addresses[1] = address2;
     }
 
+    // creates and returns a new TicTacToeLockedState
     function createLockedState() internal returns (LockedState) {
         return new TicTacToeLockedState();
     }
