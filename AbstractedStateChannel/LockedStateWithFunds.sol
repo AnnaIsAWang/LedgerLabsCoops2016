9160ae26263d21a91f37441ea2b28d1634f39087
Denton Liu <liu.denton+github@gmail.com>
2016-05-30 11:11:28 -0400

    Commented LockedStateWithFunds.sol

--- AbstractedStateChannel/LockedStateWithFunds.sol
+++ AbstractedStateChannel/LockedStateWithFunds.sol
@@ -1,11 +1,23 @@
 import "LockedState.sol";
 
+/**
+ * LockedStateWithFunds
+ *
+ * LockedState which implements the tracking of funds sent to it.
+ */
 contract LockedStateWithFunds is LockedState {
 
+    // Event triggered whenever funds have been added
     event FundsAdded(address from, address to, uint amount);
 
     mapping (address => uint) balances;
 
+    /**
+     * Gets the balance of the address passed in.
+     *
+     * toCheck: the address to check the balance for
+     * returns: the balance
+     */
     function getBalance(address toCheck) constant returns (uint) {
         return balances[toCheck];
     }
