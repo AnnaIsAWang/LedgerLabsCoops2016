4c6f0d1561092752c6a016b05bbcbc2d34e5b213
Denton Liu <liu.denton+github@gmail.com>
2016-05-30 11:20:35 -0400

    Added comments for Rules.sol

--- AbstractedStateChannel/Rules.sol
+++ AbstractedStateChannel/Rules.sol
@@ -1,17 +1,25 @@
 import "Adjudicator.sol";
 import "LockedState.sol";
 
+/**
+ * Rules
+ *
+ * The rules of the state channel. Will be further implemented by any inheriting contracts.
+ */
 contract Rules {
 
+    // Event triggered when a new state channel is created (this contract is initialised)
     event StateChannelCreated(address Adjudicator, address LockedState);
 
     Adjudicator adjudicator;
 
+    // Creates a new Rules
     function Rules() {
         adjudicator = createAdjudicator();
         StateChannelCreated(adjudicator, adjudicator.getLockedStateAddress());
     }
 
+    // An abstract method which create and return an Adjudicator. The Adjudicator will be owned by this contract.
     function createAdjudicator() internal returns (Adjudicator);
 
     // you may define the rules and exposure conditions of the state channel
