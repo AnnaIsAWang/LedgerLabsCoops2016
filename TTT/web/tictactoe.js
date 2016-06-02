af368af7a57136e7c93a2ee30c2f9df088d670d3
Anna <ay7wang@uwaterloo.ca>
2016-06-02 11:26:05 -0400

    Some documentation

--- TTT/web/tictactoe.js
+++ TTT/web/tictactoe.js
@@ -1,5 +1,6 @@
 var contractAddress = ""; // So this obviously needs to be filled in
 
+// Set up web3 component
 if (typeof web3 !== 'undefined') {
 	web3 = new Web3(web3.currentProvider);
 } else {
@@ -9,10 +10,12 @@ if (typeof web3 !== 'undefined') {
 
 var ttt = web3.eth.contract(ABI).at(contractAddress); // ABI needs to be a real ABI
 
+// For creating a new game, you need an ethereum account to do this
 function newGame(form) {
 	var gameID = ttt.newGame.sendTransaction({from: web3.eth.accounts[0], value: form.bet.value * 10 ** 18});
 }
 
+// Adds any game you accept to your currently active games, still need an ethereum account to do this
 function joinGame(gameID) {
 	ttt.accept.sendTransaction(gameID, {from: web3.eth.accounts[0], value: ttt.games[gameID].bet);
 	if (ttt.games[gameID].active) {
@@ -20,6 +23,7 @@ function joinGame(gameID) {
 	}
 }
 
+// Lists all the active offers currently availible to call
 function list() {
 	window.open();
 	document.write('<center>');
@@ -31,6 +35,7 @@ function list() {
 		document.write('<input type="button" value="Accept" onClick="joinGame('+info[k]+')>');
 }
 
+// Updates the grid so that even if the page is refreshed, the buttons will be the same
 function updateGrid(grid, gameID) {
 	for (var i = 0; i < ttt.gridSize; ++i) {
 		for (var j = 0; j < ttt.gridSize; ++j) {
@@ -38,14 +43,17 @@ function updateGrid(grid, gameID) {
 				document.write('<input type="button" id='+(j + ttt.gridSize * i)+' onClick="move(this.id, this.value, gameID)" value=" "'+'<br>');
 			} else if (grid[j + ttt.gridSize * i] == 1) {
 				document.write('<input type="button" id='+(j + ttt.gridSize * i)+' value="X"'+'<br>');
+				// Clicking this should do nothing
 			} else if (grid[j + ttt.gridSize * i] == 2) {
 				document.write('<input type="button" id='+(j + ttt.gridSize * i)+' value="O"'+'<br>');
+				// Clicking this should do nothing
 			}
 		}
 		document.write('<br>');
 	}
 }
 
+// Opens a new window with all of your active games
 function blocks() {
 	window.open();
 	document.write('<center>');
@@ -60,6 +68,7 @@ function blocks() {
 	}
 }
 
+// Translates the button click to a move on the board in the blockchain
 function move(index, value, gameID) {
 	if (ttt.winner.sendTransaction(index, gameID, {from: web3.eth.accounts[0]})) {
 		value = getLetter(gameID);
