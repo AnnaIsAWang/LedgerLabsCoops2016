62e4c4d87fbf0ba72ce975166aee90c5f4e6882d
Denton Liu <liu.denton+github@gmail.com>
2016-06-02 17:03:56 -0400

    Fixed all compiler errors but one

--- AbstractedStateChannel/TicTacToeRules.sol
+++ AbstractedStateChannel/TicTacToeRules.sol
@@ -59,31 +59,27 @@ contract TicTacToeRules is Rules {
         return adjudicator.close(2, state, nonce, v, r, s);
     }
 
-    function unilateralRuling(uint8 uintState, uint nonce, uint sender) internal returns (bool) {
+    function unilateralRuling(uint8 uintState, uint nonce) internal returns (bool) {
         bytes memory state = new bytes(1);
         state[0] = byte(uintState);
         adjudicator.close(0, state, nonce, new uint8[](1), new bytes32[](1), new bytes32[](1));
     }
 
     function sendBoard(
-        uint sender,
         bytes10 board,
         uint nonce,
-        uint8 vX,
-        bytes32 rX,
-        bytes32 sX,
-        uint8 vO,
-        bytes32 rO,
-        bytes32 sO
+        uint8 v,
+        bytes32 r,
+        bytes32 s
     ) external returns (bool) {
         uint x;
         uint y;
         uint i;
-        uint8 uintState = sender == X ? 0x28 : 0x14;
+        uint8 uintState = uint(board[9]) == X ? 0x28 : 0x14;
 
         if (
-            !((uint(board[9]) == X && addressX == ecrecover(sha3(sender, board, nonce), vX, rX, sX))
-            || (uint(board[9]) == O && addressO == ecrecover(sha3(sender, board, nonce), vO, rO, sO)))
+            !((uint(board[9]) == X && addressX == ecrecover(sha3(board, nonce), v, r, s))
+            || (uint(board[9]) == O && addressO == ecrecover(sha3(board, nonce), v, r, s)))
         ) {
             return false;
         }
@@ -98,9 +94,9 @@ contract TicTacToeRules is Rules {
                 i += uint(board[gridToIndex(x, y)]);
             }
             if (i == X * 3) {
-                return unilateralRuling(uintState | 0x01 , nonce, sender);
+                return unilateralRuling(uintState | 0x01 , nonce);
             } else if (i == O * 3) {
-                return unilateralRuling(uintState | 0x02, nonce, sender);
+                return unilateralRuling(uintState | 0x02, nonce);
             }
         }
 
@@ -111,9 +107,9 @@ contract TicTacToeRules is Rules {
                 i += uint(board[gridToIndex(x, y)]);
             }
             if (i == X * 3) {
-                return unilateralRuling(uintState | 0x01, nonce, sender);
+                return unilateralRuling(uintState | 0x01, nonce);
             } else if (i == O * 3) {
-                return unilateralRuling(uintState | 0x02, nonce, sender);
+                return unilateralRuling(uintState | 0x02, nonce);
             }
         }
 
@@ -123,9 +119,9 @@ contract TicTacToeRules is Rules {
             i += uint(board[gridToIndex(x, x)]);
         }
         if (i == X * 3) {
-            return unilateralRuling(uintState | 0x01, nonce, sender);
+            return unilateralRuling(uintState | 0x01, nonce);
         } else if (i == O * 3) {
-            return unilateralRuling(uintState | 0x02, nonce, sender);
+            return unilateralRuling(uintState | 0x02, nonce);
         }
 
         // checking /
@@ -134,47 +130,74 @@ contract TicTacToeRules is Rules {
             i += uint(board[gridToIndex(x, 2 - x)]);
         }
         if (i == X * 3) {
-            return unilateralRuling(uintState | 0x01, nonce, sender);
+            return unilateralRuling(uintState | 0x01, nonce);
         } else if (i == O * 3) {
-            return unilateralRuling(uintState | 0x02, nonce, sender);
+            return unilateralRuling(uintState | 0x02, nonce);
         }
 
         // check if tie
         for (i = 0; i < 9; i++) {
-            if (board[i] == BLANK) {
+            if (uint(board[i]) == BLANK) {
                 // last player wins
-                return unilateralRuling(uintState | uint(board[9]) == X ? 0x01 : 0x02, nonce, sender);
+                return unilateralRuling(uintState | uint(board[9]) == X ? 0x01 : 0x02, nonce);
             }
         }
         // it is a tie
-        return unilateralRuling(uintState, nonce, sender);
+        return unilateralRuling(uintState, nonce);
     }
 
-    function badBoardSent() {
-        // implement the signature checking and all that fun bullshit over here please
-        // oldBoard and newBoard???
+    function badBoardSent(
+        bytes10 oldBoard,
+        uint oldNonce,
+        uint8 oldV,
+        bytes32 oldR,
+        bytes32 oldS,
+        bytes10 newBoard,
+        uint newNonce,
+        uint8 newV,
+        bytes32 newR,
+        bytes32 newS
+    ) external returns (bool) {
+        if (
+            !((uint(oldBoard[9]) == X && addressX == ecrecover(sha3(oldBoard, oldNonce), oldV, oldR, oldS))
+            || (uint(oldBoard[9]) == O && addressO == ecrecover(sha3(oldBoard, oldNonce), oldV, oldR, oldS)))
+        ) {
+            return false;
+        }
+
+        if (
+            !((uint(newBoard[9]) == X && addressX == ecrecover(sha3(newBoard, newNonce), newV, newR, newS))
+            || (uint(newBoard[9]) == O && addressO == ecrecover(sha3(newBoard, newNonce), newV, newR, newS)))
+        ) {
+            return false;
+        }
+
         bool notChanged = true;
         for (uint i = 0; i < 9; i++) {
             if (oldBoard[i] == newBoard[i]) {
                 break;
             }
-            if ((newBoard[i] == X || newBoard[i] == O) && (oldBoard[i] != newBoard[i]) && (oldBoard[i] == BLANK) && (notChanged && newBoard[i] == newBoard[9])) {
+            if ((uint(newBoard[i]) == X || uint(newBoard[i]) == O) && (oldBoard[i] != newBoard[i]) && (uint(oldBoard[i]) == BLANK) && (notChanged && newBoard[i] == newBoard[9])) {
                 notChanged = false;
                 break;
             }
             // shenanigans
+            return unilateralRuling(uint(newBoard[9]) == X ? 0x56 : 0x69, newNonce);
         }
         if (notChanged) {
-            // shenanigans!
+            // shenanigans
+            return unilateralRuling(uint(newBoard[9]) == X ? 0x56 : 0x69, newNonce);
         } else {
-            // no shenanigans
+            // nothing fishy
+            return false;
         }
     }
 
-    function checkIn(uint player, uint nonce) {
-        uint8 uintState = uint8(adjudicator.getState()[0]);
-        if (nonce == adjudicator.getNonce() && uintState & 0x40 == 0) {
-            uintState = uintState & 0xCF | 0x0C;
+    function checkIn() external returns (bool) {
+        uint8 uintState = uint8(adjudicator.getState()[0]);// this line doesn't compile???
+        if (uintState & 0x40 != 0x00 || uintState & 0x0C != 0x00) {
+            return false;
         }
+        return unilateralRuling(uintState & 0xCF | 0x0C, adjudicator.getNonce());
     }
 }
