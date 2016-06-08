25024b791cf3bd1314aa2a8d7489239a529821a1
AnnaIsAWang <s067220898@gmail.com>
2016-06-08 14:12:45 -0400

    forgot a web3 before my eth

--- AbstractedStateChannel/parametersForEcrecover.js
+++ AbstractedStateChannel/parametersForEcrecover.js
@@ -2,7 +2,7 @@
 
 function signature(data, nonce, rulesAddress) {
 	var hash = web3.sha3(data, nonce, rulesAddress);
-	var sig = web3.eth.sign(eth.accounts[0], hash);
+	var sig = web3.eth.sign(web3.eth.accounts[0], hash);
 	var r = "0x" + sig.slice(2, 66);
 	var s = "0x" + sig.slice(66, 130);
 	var v = 27 + Number(sig.slice(130, 132));
